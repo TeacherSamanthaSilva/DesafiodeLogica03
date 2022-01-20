86767910e7ba3f65ac3e8e4d82876e3897bea2df
TeacherSamanthaSilva <samanthaadelinecordova@outlook.com>
2022-01-20 11:11:22 -0300

    while
    Nesse commit foi criado o laço de repetição while

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
+// laço de repetição for
+
 let i 
 
 for(i=0;i<=10;i++){
     console.log(i);
+}
+
+// laço de repetição while
+
+let contador
+contador = 0;
+
+while(contador <=10){
+    console.log(contador);
+    contador++;
 }
